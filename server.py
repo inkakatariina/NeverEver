41189107df8f54a8204219ac9bff0bfa40fb1ad5
inkakatariina <157373215+inkakatariina@users.noreply.github.com>
2025-05-08 20:19:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import eventlet
 eventlet.monkey_patch()
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-
 @app.route("/api/games", methods=["POST"])
 def create_game():
     print("Game created")
     return {"message": "Game created"}
+
+@app.route("/game")
+def game():
+    return render_template("game.html")  # Muista tehdä tämä tiedosto kansioon templates/
 
 if __name__ == '__main__':
     socketio.run(app)
