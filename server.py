1509366fa39d540a3f014fffb62487f59a55e31b
inkakatariina <157373215+inkakatariina@users.noreply.github.com>
2025-05-08 16:45:23 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/api/games", methods=["POST"])
+def create_game():
+    print("Game created")
+    return {"message": "Game created"}
+
+if __name__ == '__main__':
+    socketio.run(app)
